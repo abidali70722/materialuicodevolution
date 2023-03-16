f8fe129fdec083478a5185c3ac226df19c2477de
abidali70722 <abidali70722@gmail.com>
2023-03-16 23:42:12 +0530

    Typography button changes

---
+++ react-mui-demo/src/components/MuiButton.tsx
@@ -0,0 +1,39 @@
+import { Button, Stack, IconButton } from '@mui/material'
+import React from 'react' 
+import SendIcon from '@mui/icons-material/Send';
+export const MuiButton = () => {
+  return (
+      <Stack>
+          <Stack spacing={2} direction="row">
+              <Button variant='text' color="primary"  href='https://google.com'>Text</Button>
+              <Button variant='contained' color="error">Contained</Button>
+              <Button variant='outlined'>Outlined</Button>
+          </Stack>
+          <Stack display="block" spacing={2} direction="row" >
+              <Button variant='contained' color="primary">Primary</Button>
+              <Button variant='contained' color="secondary">Secondary</Button>
+              <Button variant='contained' color="error">Error</Button>
+              <Button variant='contained' color="warning">Warning</Button>
+              <Button variant='contained' color="info">Info</Button>
+              <Button variant='contained' color="success">Success</Button>
+          </Stack>
+          <Stack display="block" spacing={2} direction="row">
+            <Button variant='contained' size='small'>Small</Button>
+            <Button variant='contained' size='medium'>Medium</Button>
+            <Button variant='contained' size='large'>Large</Button>
+          </Stack>
+          {/* <Stack display="block" spacing={2} direction="row">
+            <Button variant='contained' startIcon={<SendIcon />}>Send</Button> 
+            <Button variant='contained' endIcon={<SendIcon />}>Send</Button> 
+          </Stack> */}
+
+          {/* <Stack spacing={2} direction='row'>
+                <IconButton aria-label="send" >
+                    <SendIcon />
+                </IconButton>
+          </Stack> */}
+
+
+      </Stack>
+  )
+}
